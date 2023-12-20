dabda1cd702fc32677088ce2abc8f69649ef4522
Michael Amadi <amadimichaeld@gmail.com>
2023-12-20 01:51:42 +0100

    fix median, use bloom filter for node signer uniqueness and more

--- script/median.s.sol
+++ script/median.s.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.21;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {Median} from "../src/median.sol";
-import {Proxy} from "../src/proxy.sol";
 
 contract MedianScript is Script {
     function setUp() public {}
@@ -13,19 +12,12 @@ contract MedianScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         console2.logString("Deploying median implementation");
-        Median median = new Median();
-        string memory logMessage = string.concat("Median implementation address: ", vm.toString(address(median)));
-        console2.logString(logMessage);
-
-        bytes memory initializeData = abi.encodeCall(Median.initialize, (1)); // minimumQuorum = 1
-
-        console2.logString("Deploying proxy and pointing it to implementation");
-        Proxy proxy = new Proxy(address(median), initializeData);
-        logMessage = string.concat("Proxy address: ", vm.toString(address(proxy)));
+        Median median = new Median(1, address(1234), address(5678));
+        string memory logMessage = string.concat("Median contract address: ", vm.toString(address(median)));
         console2.logString(logMessage);
 
         // for testing with local relayer client
-        // Median(address(proxy)).authorizeRelayer(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        median.authorizeNode(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
         vm.stopBroadcast();
     }
